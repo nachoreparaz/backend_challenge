b39b36996263a35f9936e3b8b7071491734c58a4
ignacio <ignacio.reparaz@fluxit.com.ar>
2025-03-20 04:15:04 -0300

    update type response services

--- src/services/contact.ts
+++ src/services/contact.ts
@@ -11,22 +11,22 @@ export default class ContactService {
     try {
       return await this.#repository.retrieveById(id);
     } catch (error) {
-      return error;
+      return error as Error;
     }
   }
   create = async (contact: IContactCreationBody) => {
     try {
       return await this.#repository.createContact(contact);
     } catch (error) {
-      return error;
+      return error as Error;
     }
   }
 
-  update = async (id: number, contact: IContactUpdateBody) => {
+  update = async (id: number, contact: IContactUpdateBody): Promise<void | Error> => {
     try {
       await this.#repository.updateContact(id, contact);
     } catch (error) {
-      return error;
+      return error as Error;
     }
   }
 
@@ -34,15 +34,15 @@ export default class ContactService {
     try {
       await this.#repository.deleteContact(id);
     } catch (error) {
-      return error;
+      return error as Error;
     }
   }
 
-  customFindAll = async (strategy: QueryStrategy) => {
+  customFindAll = async (strategy: QueryStrategy)=> {
     try {
       return await this.#repository.customRetrieveAll(strategy);
     } catch (error) {
-      return error;
+      return error as Error;
     }
   }
 
@@ -50,7 +50,7 @@ export default class ContactService {
     try {
       return await this.#repository.customRetrieveOne(strategy);
     } catch (error) {
-      return error;
+      return error as Error;
     }
   }
 
